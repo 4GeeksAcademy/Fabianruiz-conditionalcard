09eac9bfaf846f1f3168cec62a05ae7b5d8cd91e
fabruizb <166636528+fabruizb@users.noreply.github.com>
2024-07-16 20:53:02 +0000

    Pending changes exported from your codespace

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,23 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let positionClass =
+    variables.socialMediaPosition === "position-right"
+      ? "position-right"
+      : "position-left";
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.country} ${variables.city}</h3>
+          <ul class="${positionClass}">
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i><h3> ${variables.twitter} </h3></a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i><h3> ${variables.github} </h3></a></li>
+            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i><h3> ${variables.linkedin} </h3></a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i><h3> ${variables.instagram} </h3></a></li>
           </ul>
         </div>
     `;
